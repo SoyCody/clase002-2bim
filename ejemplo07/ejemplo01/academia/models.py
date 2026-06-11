facf56c11dd349cc3877ca34474b74b1f6a1dd53
Cody <codycabreraa@gmail.com>
2026-06-11 12:12:51 -0500

    CedulaEstudiantes

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,18 @@ class Estudiante(models.Model):
     cedula = models.CharField(max_length=30, unique=True)
     edad = models.IntegerField()
 
-
     def __str__(self):
-        return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - CI: {self.obtener_cedula()} - Edad: {self.edad} - Año de Nacimiento: {self.obtener_nacimiento()}"
+   
+    def obtener_nacimiento(self):
+        anio_actual =datetime.now().year
+        anio_nacimiento = anio_actual - self.edad
+        return anio_nacimiento
+    
+    def obtener_cedula(self):
+        provincias={
+            11: "Loja",
+            19: "Zamora Chinchipe"}
+        codigo=self.cedula[:2]
+        provincia=provincias.get(int(codigo), "Desconocida")
+        return provincia
